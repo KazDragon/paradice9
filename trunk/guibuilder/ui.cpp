62c9c35555d39d073710c1b403f642b104b57e99
matthew.chaplain <matthew.chaplain@ef7115a4-cac6-11de-a9c8-2fbcfba63733>
2011-07-21 20:18:55 +0000

    First pass at a list component.

--- trunk/guibuilder/ui.cpp
+++ trunk/guibuilder/ui.cpp
@@ -35,6 +35,7 @@
 #include "munin/image.hpp"
 #include "munin/edit.hpp"
 #include "munin/framed_component.hpp"
+#include "munin/list.hpp"
 #include "munin/scroll_pane.hpp"
 #include "munin/solid_frame.hpp"
 #include "munin/text_area.hpp"
@@ -51,228 +52,22 @@ using namespace std;
 
 namespace guibuilder {
 
-static string main_image[] = {
-    
-    "             _"
-  , "    |/ _._  | \\.__. _  _ ._/ _"
-  , "    |\\(_|/_ |_/|(_|(_|(_)| |_>"
-  , "                    _|"
-  , " "
-  , "                   ___                   ___           ___"
-  , "                  / _ \\___ ________ ____/ (_)______   / _ \\"
-  , "                 / ___/ _ `/ __/ _ `/ _  / / __/ -_)  \\_, /"
-  , "                /_/   \\_,_/_/  \\_,_/\\_,_/_/\\__/\\__/  /___/"
-  , "                                                           v1.0"
-  , "                                                               "
-};
-
-BOOST_STATIC_CONSTANT(u8, SLIDER_STEP = 10);
-
-void on_left(weak_ptr<horizontal_scroll_bar> weak_scroll_bar)
-{
-    BOOST_AUTO(scroll_bar, weak_scroll_bar.lock());
-
-    if (scroll_bar != NULL)
-    {
-        BOOST_AUTO(slider_position, scroll_bar->get_slider_position().get());
-
-        scroll_bar->set_slider_position(
-            slider_position < SLIDER_STEP
-          ? 0
-          : slider_position - SLIDER_STEP);
-    }
-}
-
-void on_right(weak_ptr<horizontal_scroll_bar> weak_scroll_bar)
-{
-    BOOST_AUTO(scroll_bar, weak_scroll_bar.lock());
-
-    if (scroll_bar != NULL)
-    {
-        BOOST_AUTO(slider_position, scroll_bar->get_slider_position().get());
-
-        scroll_bar->set_slider_position(
-            slider_position > (100 - SLIDER_STEP)
-          ? 100
-          : slider_position + SLIDER_STEP);
-    }
-}
-
-void on_down(weak_ptr<vertical_scroll_bar> weak_scroll_bar)
-{
-    BOOST_AUTO(scroll_bar, weak_scroll_bar.lock());
-
-    if (scroll_bar != NULL)
-    {
-        BOOST_AUTO(slider_position, scroll_bar->get_slider_position().get());
-
-        scroll_bar->set_slider_position(
-            slider_position > (100 - SLIDER_STEP)
-          ? 100
-          : slider_position + SLIDER_STEP);
-    }
-}    
-
-void on_up(weak_ptr<vertical_scroll_bar> weak_scroll_bar)
-{
-    BOOST_AUTO(scroll_bar, weak_scroll_bar.lock());
-
-    if (scroll_bar != NULL)
-    {
-        BOOST_AUTO(slider_position, scroll_bar->get_slider_position().get());
-
-        scroll_bar->set_slider_position(
-            slider_position < SLIDER_STEP
-          ? 0
-          : slider_position - SLIDER_STEP);
-    }
-}
-
 ui::ui()
 {
     BOOST_AUTO(container, get_container());
     container->set_layout(make_shared<grid_layout>(1, 1));
 
-    /*
-    glyph plusglyph(
-        char(206)
-      , odin::ansi::character_set::CHARACTER_SET_G0
-      , odin::ansi::character_set::LOCALE_SCO);
-
-    attribute pen;
-    pen.foreground_colour_ = odin::ansi::graphics::COLOUR_RED;
-    pen.intensity_         = odin::ansi::graphics::INTENSITY_BOLD;
-
-    element_type element(plusglyph, pen);
-
-    BOOST_AUTO(block, make_shared<sized_block>(element, munin::extent(120, 40)));
-    */
-
-    BOOST_AUTO(text, make_shared<text_area>());
-
-    container->add_component(make_shared<scroll_pane>(text));
-    /*
-    BOOST_AUTO(hscrollbar, make_shared<horizontal_scroll_bar>());
-    hscrollbar->on_page_left.connect(
-        bind(on_left, weak_ptr<horizontal_scroll_bar>(hscrollbar)));
-    hscrollbar->on_page_right.connect(
-        bind(on_right, weak_ptr<horizontal_scroll_bar>(hscrollbar)));
-
-    glyph tlglyph(
-        char(201)
-      , odin::ansi::character_set::CHARACTER_SET_G0
-      , odin::ansi::character_set::LOCALE_SCO);
-    
-    element_type tlelement(tlglyph, attribute());
-    element_type tlelements[] = { tlelement };
-    BOOST_AUTO(tlcorner, make_shared<image>(tlelements));
-    
-    glyph trglyph(
-        char(187)
-      , odin::ansi::character_set::CHARACTER_SET_G0
-      , odin::ansi::character_set::LOCALE_SCO);
-    
-    element_type trelement(trglyph, attribute());
-    element_type trelements[] = { trelement };
-    BOOST_AUTO(trcorner, make_shared<image>(trelements));
-    
-    BOOST_AUTO(top_panel, make_shared<basic_container>());
-    top_panel->set_layout(make_shared<compass_layout>());
-    top_panel->add_component(
-        tlcorner
-      , COMPASS_LAYOUT_WEST);
-    top_panel->add_component(
-        hscrollbar
-      , COMPASS_LAYOUT_CENTRE);
-    top_panel->add_component(
-        trcorner
-      , COMPASS_LAYOUT_EAST);
-    
-    container->add_component(
-        top_panel
-      , COMPASS_LAYOUT_NORTH);
-    
-    BOOST_AUTO(vscrollbar, make_shared<vertical_scroll_bar>());
-    vscrollbar->on_page_up.connect(
-        bind(on_up, weak_ptr<vertical_scroll_bar>(vscrollbar)));
-    vscrollbar->on_page_down.connect(
-        bind(on_down, weak_ptr<vertical_scroll_bar>(vscrollbar)));
-    
-    container->add_component(
-        vscrollbar
-      , COMPASS_LAYOUT_WEST);
-    */
-    
-   
-    /*
- 
-    container->set_layout(make_shared<compass_layout>());
-    
-    BOOST_AUTO(input_container, make_shared<basic_container>());
-    input_container->set_layout(make_shared<compass_layout>());
-    
-    BOOST_AUTO(labels_container, make_shared<basic_container>());
-    labels_container->set_layout(make_shared<grid_layout>(2, 1));
-    
-    BOOST_AUTO(fields_container, make_shared<basic_container>());
-    fields_container->set_layout(make_shared<grid_layout>(2, 1));
-    fields_container->add_component(
-        make_shared<framed_component>(
-            make_shared<solid_frame>(), make_shared<edit>()));
-    
-    BOOST_AUTO(password_field, make_shared<edit>());
-    element_type password_element;
-    password_element.first = '*';
-    password_element.second.foreground_colour =
-        odin::ansi::graphics::COLOUR_RED;
-    password_field->set_attribute(
-        EDIT_PASSWORD_ELEMENT
-      , password_element);
-    
-    fields_container->add_component(make_shared<framed_component>(
-        make_shared<solid_frame>(), password_field));
-          
-    alignment_data alignment = {
-        HORIZONTAL_ALIGNMENT_RIGHT
-      , VERTICAL_ALIGNMENT_CENTRE
+    static string const names[] = {
+        "Item #1"
+      , "Item #2"
+      , "Item #3"
     };
-    
-    BOOST_AUTO(name_container, make_shared<basic_container>());
-    name_container->set_layout(make_shared<aligned_layout>());
-    
-    string name_image[] = { "name:" };
-    name_container->add_component(
-        make_shared<image>(image_from_text(name_image))
-      , alignment);
-    
-    BOOST_AUTO(password_container, make_shared<basic_container>());
-    password_container->set_layout(make_shared<aligned_layout>());
-    
-    string password_image[] = { "password:" };
-    password_container->add_component(
-        make_shared<image>(image_from_text(password_image))
-      , alignment);
-    
-    labels_container->add_component(name_container);
-    labels_container->add_component(password_container);
-    
-    input_container->add_component(
-        labels_container, COMPASS_LAYOUT_WEST);
-    input_container->add_component(
-        fields_container, COMPASS_LAYOUT_CENTRE);
-    
-    BOOST_AUTO(image_container, make_shared<basic_container>());
-    image_container->set_layout(make_shared<aligned_layout>());
-    
-    alignment.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTRE;
-    alignment.vertical_alignment   = VERTICAL_ALIGNMENT_CENTRE;
-    image_container->add_component(
-        make_shared<image>(image_from_text(main_image))
-      , alignment);
-    
-    container->add_component(input_container, COMPASS_LAYOUT_SOUTH);
-    container->add_component(image_container, COMPASS_LAYOUT_CENTRE);
-    */
+
+    BOOST_AUTO(name_list, make_shared<munin::list>());
+    name_list->set_items(list_from_text(names));
+    name_list->set_item_index(0);
+
+    container->add_component(name_list);
 }
 
 void ui::do_event(any const &ev)
---
+++ trunk/munin/list.hpp
@@ -0,0 +1,115 @@
+// ==========================================================================
+// Munin List Component.
+//
+// Copyright (C) 2011 Matthew Chaplain, All Rights Reserved.
+//
+// Permission to reproduce, distribute, perform, display, and to prepare
+// derivitive works from this file under the following conditions:
+//
+// 1. Any copy, reproduction or derivitive work of any part of this file 
+//    contains this copyright notice and licence in its entirety.
+//
+// 2. The rights granted to you under this license automatically terminate
+//    should you attempt to assert any patent claims against the licensor 
+//    or contributors, which in any way restrict the ability of any party 
+//    from using this software or portions thereof in any form under the
+//    terms of this license.
+//
+// Disclaimer: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+//             KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//             WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+//             PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+//             OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+//             OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+//             OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//             SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// ==========================================================================
+#ifndef MUNIN_LIST_HPP_
+#define MUNIN_LIST_HPP_
+
+#include "munin/basic_component.hpp"
+#include "odin/runtime_array.hpp"
+#include <boost/optional.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace munin {
+
+//* =========================================================================
+/// \brief A class that models a list of elements.
+//* =========================================================================
+class list : public munin::basic_component
+{
+public :
+    //* =====================================================================
+    /// \brief Constructor
+    //* =====================================================================
+    list();
+
+    //* =====================================================================
+    /// \brief Destructor
+    //* =====================================================================
+    virtual ~list();
+
+    //* =====================================================================
+    /// \brief Sets the items in the list.
+    //* =====================================================================
+    void set_items(
+        odin::runtime_array< 
+            odin::runtime_array<element_type> 
+        > const &items);
+
+    //* =====================================================================
+    /// \brief Selects an item with the given index.
+    /// \param selected_item the item to select, or -1 for no item.
+    //* =====================================================================
+    void set_item_index(odin::s32 index);
+
+    //* =====================================================================
+    /// \brief Gets the index of the currently selected item, or -1 if no 
+    /// item is selected.
+    //* =====================================================================
+    odin::s32 get_item_index() const;
+
+    //* =====================================================================
+    /// \brief Gets the value of the currently selected item.
+    //* =====================================================================
+    odin::runtime_array<element_type> get_item() const;
+
+protected :
+    //* =====================================================================
+    /// \brief Called by get_preferred_size().  Derived classes must override
+    /// this function in order to get the size of the component in a custom 
+    /// manner.
+    //* =====================================================================
+    virtual extent do_get_preferred_size() const;
+    
+    //* =====================================================================
+    /// \brief Called by draw().  Derived classes must override this function
+    /// in order to draw onto the passed canvas.  A component must only draw 
+    /// the part of itself specified by the region.
+    ///
+    /// \param cvs the canvas in which the component should draw itself.
+    /// \param region the region relative to this component's origin that
+    /// should be drawn.
+    //* =====================================================================
+    virtual void do_draw(
+        canvas          &cvs
+      , rectangle const &region);
+    
+private :
+    struct impl;
+    boost::shared_ptr<impl> pimpl_;
+};
+
+//* =========================================================================
+/// \brief Converts a runtime_array<> of strings into a runtime_array<> of
+/// runtime_array<>s of ANSI elements that are suitable for use with the
+/// munin::ansi::list component.
+//* =========================================================================
+odin::runtime_array< odin::runtime_array<element_type> >
+    list_from_text(odin::runtime_array<std::string> const &text);
+
+}
+
+#endif
+

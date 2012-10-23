088bcf3dcaba01a17d60c2e1b6e93c17525627cb
matthew.chaplain <matthew.chaplain@ef7115a4-cac6-11de-a9c8-2fbcfba63733>
2012-10-23 05:26:44 +0000

    Added munin::clock, Paradice's first animated component.

---
+++ munin/clock.hpp
@@ -0,0 +1,71 @@
+// ==========================================================================
+// Munin Clock.
+//
+// Copyright (C) 2012 Matthew Chaplain, All Rights Reserved.
+//
+// Permission to reproduce, distribute, perform, display, and to prepare
+// derivitive works from this file under the following conditions:
+//
+// 1. Any copy, reproduction or derivitive work of any part of this file 
+//    contains this copyright notice and licence in its entirety.
+//
+// 2. The rights granted to you under this license automatically terminate
+//    should you attempt to assert any patent claims against the licensor 
+//    or contributors, which in any way restrict the ability of any party 
+//    from using this software or portions thereof in any form under the
+//    terms of this license.
+//
+// Disclaimer: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+//             KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//             WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+//             PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+//             OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+//             OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+//             OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//             SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// ==========================================================================
+#ifndef MUNIN_clock_HPP_
+#define MUNIN_clock_HPP_
+
+#include "munin/composite_component.hpp"
+
+namespace munin {
+
+//* =========================================================================
+/// \brief A class that models a clock.
+//* =========================================================================
+class clock 
+  : public munin::composite_component
+{
+public :
+    //* =====================================================================
+    /// \brief Constructor
+    //* =====================================================================
+    clock();
+
+    //* =====================================================================
+    /// \brief Destructor
+    //* =====================================================================
+    virtual ~clock();
+    
+protected :    
+    //* =====================================================================
+    /// \brief Called by draw().  Derived classes must override this function
+    /// in order to draw onto the passed context.  A component must only draw 
+    /// the part of itself specified by the region.
+    ///
+    /// \param ctx the context in which the component should draw itself.
+    /// \param region the region relative to this component's origin that
+    /// should be drawn.
+    //* =====================================================================
+    virtual void do_draw(
+        context         &ctx
+      , rectangle const &region);
+
+    struct impl;
+    boost::shared_ptr<impl> pimpl_;    
+};
+
+}
+
+#endif

788985b8a8cd4d3be06cbd18f23a7039ee80e0ea
Matthew Chaplain <matthew.chaplain@gmail.com>
2015-07-20 15:51:12 +0200

    Added support for array properties
    Strings are mapped to estrings, and arrays are mapped to
    vector<estring>s.
    
    At this point, the image example from Issue #145 is doable.

--- test/yggdrasil_munin_create_model_from_ptree_fixture.cpp
+++ test/yggdrasil_munin_create_model_from_ptree_fixture.cpp
@@ -2,6 +2,7 @@
 #include "yggdrasil/munin/ptree_model.hpp"
 #include "yggdrasil/munin/model.hpp"
 #include "yggdrasil/munin/basic_model.hpp"
+#include "yggdrasil/munin/estring.hpp"
 #include <boost/property_tree/json_parser.hpp>
 #include <sstream>
 
@@ -42,19 +43,23 @@ void yggdrasil_munin_create_model_from_ptree_fixture::test_ptree_with_only_name_
     CPPUNIT_ASSERT(prop.empty());
 }
 
-void yggdrasil_munin_create_model_from_ptree_fixture::test_ptree_with_string_property_creates_model_with_that_property()
+void yggdrasil_munin_create_model_from_ptree_fixture::test_ptree_with_string_property()
 {
+    using namespace yggdrasil::literals;
+    
     // The model should add properties as described in the model description.
     // In particular, given a property with a given name and a string argument,
-    // that string should be found in the model's properties.
+    // that string should be found in the model's properties.  Note: the model
+    // should store the string value as an estring.
     std::stringstream is(R"(
         { "name" : "test",
           "properties" : [
             { "name" : "strprop",
-              "value" : "success" }
+              "default" : "success" }
           ]
         }
     )");
+
     boost::property_tree::ptree tree;
     boost::property_tree::read_json(is, tree);
     
@@ -67,6 +72,51 @@ void yggdrasil_munin_create_model_from_ptree_fixture::test_ptree_with_string_pro
     
     boost::any prop = get_property(model, "strprop");
     CPPUNIT_ASSERT(!prop.empty());
-    auto value = boost::any_cast<std::string>(prop);
-    CPPUNIT_ASSERT_EQUAL(std::string("success"), value);
+    auto value = boost::any_cast<yggdrasil::munin::estring>(prop);
+    CPPUNIT_ASSERT_EQUAL("success"_es, value);
+}
+
+void yggdrasil_munin_create_model_from_ptree_fixture::test_ptree_with_array_of_string_property()
+{
+    using namespace yggdrasil::literals;
+
+    // The model should add properties as described in the model description.
+    // In particular, given a property with a given name and an argument that
+    // is an array of strings, that array should be found in the model's 
+    // properties as a vector<estring>.
+    std::stringstream is(R"(
+        { "name" : "test",
+          "properties" : [
+            { "name" : "value",
+              "default" : [
+                "o-o",
+                "-o-",
+                "x-x"
+              ]
+            }
+          ]
+        }
+    )");
+
+                
+    boost::property_tree::ptree tree;
+    boost::property_tree::read_json(is, tree);
+    
+    auto model = yggdrasil::munin::model{
+        yggdrasil::munin::create_model_from_ptree<
+            yggdrasil::munin::basic_model
+        >(tree)};
+        
+    CPPUNIT_ASSERT_EQUAL(yggdrasil::munin::extent{}, get_size(model));
+    
+    boost::any prop = get_property(model, "value");
+    CPPUNIT_ASSERT(!prop.empty());
+    auto value = boost::any_cast<std::vector<yggdrasil::munin::estring>>(prop);
+    auto expected = std::vector<yggdrasil::munin::estring>{
+        "o-o"_es,
+        "-o-"_es,
+        "x-x"_es
+    };
+
+    CPPUNIT_ASSERT(expected == value);
 }
--- test/yggdrasil_munin_create_model_from_ptree_fixture.hpp
+++ test/yggdrasil_munin_create_model_from_ptree_fixture.hpp
@@ -10,13 +10,15 @@ public :
     CPPUNIT_TEST_SUITE(yggdrasil_munin_create_model_from_ptree_fixture);
         CPPUNIT_TEST(test_empty_ptree_creates_default_model);
         CPPUNIT_TEST(test_ptree_with_only_name_creates_default_model);
-        CPPUNIT_TEST(test_ptree_with_string_property_creates_model_with_that_property);
+        CPPUNIT_TEST(test_ptree_with_string_property);
+        CPPUNIT_TEST(test_ptree_with_array_of_string_property);
     CPPUNIT_TEST_SUITE_END();
     
 public :
     void test_empty_ptree_creates_default_model();
     void test_ptree_with_only_name_creates_default_model();
-    void test_ptree_with_string_property_creates_model_with_that_property();
+    void test_ptree_with_string_property();
+    void test_ptree_with_array_of_string_property();
 };
 
 #endif

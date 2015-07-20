4f31d88cc7fed142944d022c5448b0f98727d075
Matthew Chaplain <matthew.chaplain@gmail.com>
2015-07-20 14:59:49 +0200

    Introduced a function that creates a model from a property tree.

---
+++ test/yggdrasil_munin_create_model_from_ptree_fixture.hpp
@@ -0,0 +1,22 @@
+#ifndef YGGDRASIL_MUNIN_CREATE_MODEL_FROM_PTREE_FIXTURE_HPP_
+#define YGGDRASIL_MUNIN_CREATE_MODEL_FROM_PTREE_FIXTURE_HPP_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class yggdrasil_munin_create_model_from_ptree_fixture : public CppUnit::TestFixture
+{
+public :
+    CPPUNIT_TEST_SUITE(yggdrasil_munin_create_model_from_ptree_fixture);
+        CPPUNIT_TEST(test_empty_ptree_creates_default_model);
+        CPPUNIT_TEST(test_ptree_with_only_name_creates_default_model);
+        CPPUNIT_TEST(test_ptree_with_string_property_creates_model_with_that_property);
+    CPPUNIT_TEST_SUITE_END();
+    
+public :
+    void test_empty_ptree_creates_default_model();
+    void test_ptree_with_only_name_creates_default_model();
+    void test_ptree_with_string_property_creates_model_with_that_property();
+};
+
+#endif
---
+++ yggdrasil/munin/ptree_model.hpp
@@ -0,0 +1,60 @@
+// ==========================================================================
+// Munin Property Tree Model.
+//
+// Copyright (C) 2015 Matthew Chaplain, All Rights Reserved.
+//
+// Permission to reproduce, distribute, perform, display, and to prepare
+// derivitive works from this file under the following conditions:
+//
+// 1. Any copy, reproduction or derivitive work of any part of this file
+//    contains this copyright notice and licence in its entirety.
+//
+// 2. The rights granted to you under this license automatically terminate
+//    should you attempt to assert any patent claims against the licensor
+//    or contributors, which in any way restrict the ability of any party
+//    from using this software or portions thereof in any form under the
+//    terms of this license.
+//
+// Disclaimer: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+//             KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//             WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+//             PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+//             OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//             OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+//             OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//             SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ==========================================================================
+#ifndef YGGDRASIL_MUNIN_PTREE_MODEL_HPP_
+#define YGGDRASIL_MUNIN_PTREE_MODEL_HPP_
+
+#include "yggdrasil/munin/model.hpp"
+#include <boost/property_tree/ptree.hpp>
+
+namespace yggdrasil { namespace munin {
+
+template <class ConcreteModel>
+ConcreteModel create_model_from_ptree(boost::property_tree::ptree const& tree)
+{
+    auto concrete_model = ConcreteModel{};
+    auto model = yggdrasil::munin::model{concrete_model};
+    
+    auto properties = tree.get_child_optional("properties");
+    
+    if (properties) 
+    {
+        for (auto const &entry : *properties)
+        {
+            auto const &property = entry.second;
+            auto const &name = property.get<std::string>("name");
+            auto const &value = property.get<std::string>("value");
+            
+            set_property(model, name, value);
+        }
+    }
+
+    return concrete_model;
+}
+
+}}
+
+#endif

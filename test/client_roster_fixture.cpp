e69ce1f5daa5fd9eaeea94b1aa41a952df87a1dc
PerdixOfMars <perdix.p76l@googlemail.com>
2026-04-08 18:43:17 +0000

    help: confirm permission management visibility

--- test/client_roster_fixture.cpp
+++ test/client_roster_fixture.cpp
@@ -1206,3 +1206,28 @@ TEST(a_client, includes_set_password_in_help_admin_for_a_caller_with_admin_set_p
         "Admin commands:\n/admin list_accounts\n/admin list_characters <account>\n/admin set_password <account> <password>"_ts,
         context.direct_messages[0]);
 }
+
+TEST(a_client, includes_permission_management_in_help_admin_for_a_caller_with_admin_set_permission)
+{
+    boost::asio::io_context io_context;
+    fake_context context;
+    auto channel = std::make_shared<fake_channel>();
+
+    paradice::client client(
+        io_context, context, paradice::connection(*channel), {});
+
+    drain(io_context);
+    channel->written_.clear();
+    enter_game(io_context, channel);
+    context.granted_permissions.emplace_back("account", "admin_access");
+    context.granted_permissions.emplace_back("account", "admin_set_permission");
+
+    enter_command_and_capture_messages(
+        io_context, context, channel, "/help admin");
+
+    ASSERT_EQ(1u, context.direct_messages.size());
+    ASSERT_EQ(0u, context.room_messages.size());
+    ASSERT_EQ(
+        "Admin commands:\n/admin list_accounts\n/admin list_characters <account>\n/admin set_permission <account> <permission>\n/admin clear_permission <account> <permission>"_ts,
+        context.direct_messages[0]);
+}

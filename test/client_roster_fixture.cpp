7c6ca25417b212c127ef8bf7101b6c368edcb4dd
PerdixOfMars <perdix.p76l@googlemail.com>
2026-04-08 12:52:36 +0000

    dice: confirm private roll usage errors

--- test/client_roster_fixture.cpp
+++ test/client_roster_fixture.cpp
@@ -633,3 +633,28 @@ TEST(a_client, reports_a_deterministic_private_roll_with_category_faces_and_tota
         {2, 4},
         "you roll 2d6+3 initiative and score 9 [2, 4]"_ts);
 }
+
+TEST(a_client, reports_invalid_private_roll_usage_only_to_the_sender)
+{
+    boost::asio::io_context io_context;
+    fake_context context;
+    auto channel = std::make_shared<fake_channel>();
+
+    paradice::client client(
+        io_context, context, paradice::connection(*channel), {});
+
+    drain(io_context);
+    channel->written_.clear();
+    enter_game(io_context, channel);
+    enter_command_and_capture_messages(io_context, context, channel, "/rollprivate nope");
+
+    ASSERT_EQ(1u, context.direct_messages.size());
+    ASSERT_EQ(0u, context.room_messages.size());
+    ASSERT_EQ(
+        "\n Usage:   roll [n*]<dice>d<sides>[<bonuses...>] [<category>]"
+        "\n Example: roll 2d6+3-20"
+        "\n Example: roll 20*2d6"
+        "\n Example: roll 1d10+4 initiative"
+        "\n"_ts,
+        context.direct_messages[0]);
+}

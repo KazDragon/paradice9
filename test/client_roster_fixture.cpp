12ff30b826c0c0de942a10611559522c5f34b50d
PerdixOfMars <perdix.p76l@googlemail.com>
2026-04-08 12:30:04 +0000

    dice: confirm shared roll bonus chain

--- test/client_roster_fixture.cpp
+++ test/client_roster_fixture.cpp
@@ -578,3 +578,36 @@ TEST(a_client, reports_a_deterministic_shared_roll_with_category_faces_and_total
         "Mallory rolls 2d6+3 initiative and scores 9 [2, 4]"_ts,
         context.room_messages[0]);
 }
+
+TEST(a_client, reports_a_deterministic_shared_roll_with_multiple_dice_and_bonus_chain)
+{
+    boost::asio::io_context io_context;
+    fake_context context;
+    auto channel = std::make_shared<fake_channel>();
+    auto next_face = std::size_t{0};
+
+    paradice::client client(
+        io_context,
+        context,
+        paradice::connection(*channel),
+        {},
+        [&](std::uint32_t) {
+            auto const faces = std::array<std::int32_t, 3>{1, 2, 3};
+            return faces[next_face++];
+        });
+
+    drain(io_context);
+    channel->written_.clear();
+    enter_game(io_context, channel);
+    enter_command_and_capture_messages(
+        io_context, context, channel, "/roll 3d9+3-2 initiative");
+
+    ASSERT_EQ(1u, context.direct_messages.size());
+    ASSERT_EQ(1u, context.room_messages.size());
+    ASSERT_EQ(
+        "you roll 3d9+3-2 initiative and score 7 [1, 2, 3]"_ts,
+        context.direct_messages[0]);
+    ASSERT_EQ(
+        "Mallory rolls 3d9+3-2 initiative and scores 7 [1, 2, 3]"_ts,
+        context.room_messages[0]);
+}

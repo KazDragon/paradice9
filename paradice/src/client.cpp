f8be202e1267755d43f54d2a086ad22a03595fca
PerdixOfMars <perdix.p76l@googlemail.com>
2026-04-13 16:47:12 +0000

    refactor: delegate admin set_password

--- paradice/src/client.cpp
+++ paradice/src/client.cpp
@@ -583,30 +583,12 @@ private:
             return true;
         }
 
-        if (input.starts_with("/admin set_password "))
+        if (input.starts_with("/admin set_password ")
+            && !has_active_account_permission(permissions::admin_set_password))
         {
-            if (!has_active_account_permission(permissions::admin_set_password))
-            {
-                context_.send_message(
-                    *character_,
-                    "You do not have permission to use /admin set_password");
-                return true;
-            }
-
-            auto arguments =
-                input.substr(std::string{"/admin set_password "}.size());
-            auto const split = split_two_arguments(arguments);
-
-            if (!split)
-            {
-                context_.send_message(*character_, admin_usage_message);
-                return true;
-            }
-
-            auto const &[account_name, password] = *split;
-
-            context_.set_password(account_name, password);
-            context_.send_message(*character_, "Password changed.");
+            context_.send_message(
+                *character_,
+                "You do not have permission to use /admin set_password");
             return true;
         }
 
